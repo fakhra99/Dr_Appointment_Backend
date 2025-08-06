4fd8cb6bf3a3b9360bed7d8777e387b2f5430cb7
Fakhra99 <fakhrahnajeeb61@gmail.com>
2025-08-06 23:48:13 +0500

    basic backend app

--- src/schemas/patientSchema.ts
+++
@@ -1,9 +0,0 @@
-import z from "zod";
-
-export const patientSchema = z.object({
-    name:z.string().min(1, "name is required"),
-    email:z.string().email("email is required"),
-    password:z.string().min(6, "password is required"),
-    role:z.string(),
-    image:z.string()
-})
